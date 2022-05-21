c79369112b4a91b99890901f3017dc873963c009
Reinier Joosse <Reinier.Joosse@infosupport.com>
2022-05-21 16:23:28 +0200

    Voeg properties van Obs toe

---
+++ aspnetcore-server-generated/aspnetcore-server/src/IO.Swagger/Models/Item.cs
@@ -0,0 +1,37 @@
+﻿namespace IO.Swagger.Models
+{
+    public enum Item
+    {
+        Passage = 0,
+
+        Rigid = 1,
+        
+        Wood = 2,
+        
+        Bomb = 3,
+        
+        Flames = 4,
+        
+        Fog = 5,
+        
+        ExtraBomb = 6,
+        
+        IncrRange = 7,
+        
+        Kick = 8,
+        
+        AgentDummy = 9,
+        
+        Agent0 = 10,
+        
+        Agent1 = 11,
+        
+        Agent2 = 12,
+        
+        Agent3 = 13,
+        
+        Title = 14,
+        
+        Background = 20
+    }
+}
--- aspnetcore-server-generated/aspnetcore-server/src/IO.Swagger/Models/Obs.cs
+++ aspnetcore-server-generated/aspnetcore-server/src/IO.Swagger/Models/Obs.cs
@@ -4,10 +4,77 @@ using System.Runtime.Serialization;
 
 namespace IO.Swagger.Models
 {
+    [DataContract]
     public class Obs
     {
         [Required]
+        [MaxLength(4)]
         [DataMember(Name = "alive")]
-        public IList<int> Alive { get; set; }
+        public IList<Item> Alive { get; set; }
+
+        [Required]
+        [MinLength(11)]
+        [MaxLength(11)]
+        [DataMember(Name = "board")]
+        public IList<IList<Item>> Board { get; set; }
+
+        [Required]
+        [MinLength(11)]
+        [MaxLength(11)]
+        [DataMember(Name = "bomb_blast_strength")]
+        public IList<IList<float>> BombBlastStrength { get; set; }
+
+        [Required]
+        [MinLength(11)]
+        [MaxLength(11)]
+        [DataMember(Name = "bomb_life")]
+        public IList<IList<float>> BombLife { get; set; }
+
+        [Required]
+        [MinLength(11)]
+        [MaxLength(11)]
+        [DataMember(Name = "bomb_moving_direction")]
+        public IList<IList<float>> BombMovingDirection { get; set; }
+
+        [Required]
+        [MinLength(11)]
+        [MaxLength(11)]
+        [DataMember(Name = "flame_life")]
+        public IList<IList<float>> FlameLife { get; set; }
+
+        [DataMember(Name = "game_type")]
+        public GameType? GameType { get; set; }
+
+        [DataMember(Name = "game_env")]
+        public string GameEnv { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(2)]
+        [DataMember(Name = "position")]
+        public IList<int> Position { get; set; }
+
+        [Required]
+        [DataMember(Name = "blast_strength")]
+        public int BlastStrength { get; set; }
+
+        [Required]
+        [DataMember(Name = "can_kick")]
+        public bool CanKick { get; set; }
+
+        [DataMember(Name = "teammate")]
+        public Item? Teammate { get; set; }
+
+        [Required]
+        [DataMember(Name = "ammo")]
+        public int Ammo { get; set; }
+
+        [Required]
+        [DataMember(Name = "enemies")]
+        public IList<Item> Enemies { get; set; }
+
+        [Required]
+        [DataMember(Name = "step_count")]
+        public int StepCount { get; set; }
     }
 }
